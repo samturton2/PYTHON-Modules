2108fdb23c61bee45a637be198164043c1c790ab
samturton2 <STurton@spartaglobal.com>
2020-11-09 14:13:34 +0000

    live_response.status_code added

--- python_api_with_requests.py
+++ python_api_with_requests.py
@@ -5,6 +5,6 @@ import requests
 live_response = requests.get("https://www.bbc.co.uk/")
 # it provides in integer as a response code
 
-if live_response == 200:
+if live_response.status_code == 200:
     print("mission successful!!!")
 print(live_response.status_code)
